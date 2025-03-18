1bd1b6856951c6fcc352fdfd1b0daf5306bd9ea6
Pavel <you@example.com>
2025-03-18 20:28:05 +0300

    Танки с двужующим к тебе врагом

--- Game/Scripts/Enemy.cs
+++ Game/Scripts/Enemy.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
+
+
 {
+    public string TargetTag;
+    Transform target;
+    NavMeshAgent myNavMesh;
+    float x;
+    float z;
+    void Awake()
+    {
+        myNavMesh = GetComponent<NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag(TargetTag).transform;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(TargetTag);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ApplyTarget(target);
+    }
+    void ApplyTarget(Transform targetTrans){
+        if(targetTrans != null)
+        myNavMesh.SetDestination(targetTrans.position);
     }
 }
