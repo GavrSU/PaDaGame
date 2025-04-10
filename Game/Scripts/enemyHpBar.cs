7722b9e913a2c74fba3e9f27e08b364dfd326955
Pavel <you@example.com>
2025-04-10 18:31:16 +0300

    хп бар

---
+++ Game/Scripts/enemyHpBar.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class enemyHpBar : MonoBehaviour
+{
+    public Slider healthBar; // Ссылка на UI-элемент Slider
+    private float maxHealth; // Максимальное значение здоровья
+    private float currentHealth; // Текущее значение здоровья
+
+    void Start()
+    {
+        // Инициализация здоровья
+        UpdateHealthBar();
+    }
+
+
+
+    // Метод для обновления UI-элемента
+    public void UpdateHealthBar()
+    {
+        if (healthBar != null)
+        {
+            currentHealth = gameObject.GetComponent<Health>().CurrentHp;
+            maxHealth = gameObject.GetComponent<Health>().MaxHp;
+            healthBar.value = currentHealth / maxHealth; // Устанавливаем значение слайдера
+        }
+    }
+}
